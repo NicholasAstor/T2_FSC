c8859f0b5af08f5aa419587cf12d0cbd8b16aaff
NicholasAstor <nicholas.barbosa@edu.pucrs.br>
2025-05-15 20:40:31 -0300

    Ainda preciso armazenar os valores aleatórios em algum registrador pra futuramente fazer a soma

--- t2.asm
+++ t2.asm
@@ -1,13 +1,57 @@
 .data
-hello_world: "Hello, World!\n"
+msgBemVindo: .string "Bem-vindo ao programa de teste de Assembly!\n"
+msgMaoJogador: .string "Mão: "
+msgVirgula: .string ","
+msgFechaMaoJogador: .string "]"
+msgAbreMaoJogador: .string "["
+jogador: .space 40
 
 .text
-main:
-    # Print "Hello, World!"
-    li a7, 4          # syscall for print string
-    la a0, hello_world # load address of string
-    ecall             # make syscall
+.globl main
 
-    # Exit program
-    li a7, 10         # syscall for exit
-    ecall             # make syscall
+main:
+    li a7, 4 
+    la a0, msgBemVindo
+    ecall
+    
+    li a7, 4 
+    la a0, msgMaoJogador
+    ecall
+    
+    li a7, 4
+    la a0, msgAbreMaoJogador
+    ecall
+    
+    li a7, 42
+    li a0, 0
+    li a1, 13
+    ecall
+    
+    addi a0, a0, 1
+    
+    li a7, 1
+    ecall
+    
+    li a7, 4
+    la a0,msgFechaMaoJogador
+    ecall
+    
+    li a7, 4
+    la a0, msgAbreMaoJogador
+    ecall
+    
+    li a7, 42
+    li a0, 0
+    li a1, 13
+    ecall
+    
+    addi a0, a0, 1
+    
+    li a7, 1
+    ecall
+    
+    li a7, 4
+    la a0,msgFechaMaoJogador
+    ecall
+fim:
+    j fim
