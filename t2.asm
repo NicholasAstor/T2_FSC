0913e56c17651265a2a7f24ab457846412e09c72
Rafael Dos Reis <rafaelreisgramado@gmail.com>
2025-05-15 21:27:55 -0300

    Rafa commit

--- t2.asm
+++ t2.asm
@@ -1,13 +1,51 @@
+
+
 .data
-hello_world: "Hello, World!\n"
+# Mensagens
+boas_vindas:      .string "\nBem-vindo ao Blackjack!\n"
+jogar_novamente:  .string "\nDeseja jogar novamente? (1 - Sim, 2 - Nao): "
+jogar:           .string "\n--- Jogando Blackjack ---\n"
+cartas_iniciais: .string "\nVoce e o carteador receberam 2 cartas cada.\n"
+cartas_jogador:  .string "\nVoce recebeu: "
+cartas_carteador: .string "\nCarteador recebeu: "
+nova_rodada:      .string "\n--- Nova Rodada ---\n"
+mao_jogador:      .string "Sua mao: "
+mao_carteador:    .string "Mao do carteador: "
+oculta:           .string "[ ? ]"
+soma:             .string " (Soma: " 
+opcoes:           .string "\nO que voce deseja fazer? (1 - Hit, 2 - Parar): "
+vitoria:          .string "\n*** Voce venceu! ***\n"
+derrota:          .string "\n*** Carteador venceu! ***\n"
+empate:           .string "\n*** Empate! ***\n"
+rodada_finalizada: .string "\n--- Rodada finalizada ---\n"
+obrigado:         .string "\nObrigado por jogar!\n"
+erro:            .string "\nOpcao invalida! Tente novamente.\n"
+erro_entrada:     .string "\nEntrada invalida! Tente novamente.\n"
+vez_carteador:    .string "\n--- Vez do Carteador ---\n"
+revela:           .string "Carteador revela a carta oculta: "
+estourou:         .string " Estourou!"
+blackjack:        .string " Blackjack!"
+21:              .string " 21!"
+21_carteador:     .string "Carteador tem 21!\n"
+21_jogador:       .string "Voce tem 21!\n"
+carteador_hit:    .string "carteador precisa pedir (< 17)...\n"
+carteador_parar:  .string "Carteador para (>= 17).\n"
+cartas:          .string "Cartas: "
+cartas_jogador:   .string "Cartas do jogador: "
+cartas_carteador: .string "Cartas do carteador: "
+vez_jogador:      .string "\n--- Vez do Jogador ---\n"
+recebeu:          .string "Voce recebeu: "
 
-.text
-main:
-    # Print "Hello, World!"
-    li a7, 4          # syscall for print string
-    la a0, hello_world # load address of string
-    ecall             # make syscall
 
-    # Exit program
-    li a7, 10         # syscall for exit
-    ecall             # make syscall
+# Vetores das mãos
+cartas_jogador:       .space 40  # até 10 cartas (1 word por carta)
+cartas_carteador:        .space 40
+
+num_cartas_jogador:   .word 0
+num_cartas_carteador:    .word 0
+
+soma_jogador:         .word 0
+soma_carteador:          .word 0
+
+ases_jogador:         .word 0
+ases_carteador:          .word 0
